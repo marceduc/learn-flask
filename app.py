f05a277164095fc7fe0458b1cf9c0fa3f489b127
marc <scheumar@hu-berlin.de>
2020-04-04 20:20:15 +0200

    init db

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
-import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
-# print(os.environ)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-print(os.environ['APP_SETTINGS'])
+from models import Result
 
 
 @app.route('/')
 def hello():
     return "Hello World!"
 
+
 @app.route('/<name>')
 def hello_name(name):
-    return f'Hello {name}!'
+    return "Hello {}!".format(name)
+
 
 if __name__ == '__main__':
     app.run()
